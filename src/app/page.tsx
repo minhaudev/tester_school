beba7ac80b693357554073ffa03c348afe282074
nguyenminhhau123 <hau95024@donga.edu.vn>
2024-07-11 17:28:35 +0700

    create button

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import Head from "next/head";
+export default function Home() {
+  return (
+    <>
+    <Head>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=SF+UI+Display:wght@400;700&display=swap" />
+  </Head>
+  <div className="font-sf-ui-display text-2xl font-bold">
+        SF UI Display Font
+      </div>
+  </>
+  );
+}
