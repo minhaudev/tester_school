14d5433b50af0cbcc68b2ad0de899d0937546f34
NguyenNhatVuong99 <nhatvuong0699@gmail.com>
2024-07-11 17:45:09 +0700

    test git

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,18 @@
+import Input from "@/components/atoms/Input/Input";
 import Head from "next/head";
 export default function Home() {
   return (
     <>
-    <Head>
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=SF+UI+Display:wght@400;700&display=swap" />
-  </Head>
-  <div className="font-sf-ui-display text-2xl font-bold">
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=SF+UI+Display:wght@400;700&display=swap"
+        />
+      </Head>
+      <div className="font-sf-ui-display text-2xl font-bold">
         SF UI Display Font
       </div>
-  </>
+      <Input></Input>
+    </>
   );
 }
