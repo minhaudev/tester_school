2b902b0e505879092320ae987a3c7732deb4dcf9
nguyenminhhau123 <hau95024@donga.edu.vn>
2024-07-14 16:39:54 +0700

    fix:button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,13 @@
 "use client";
-
-import Bell from "@/assets/images/Bell";
-import Breadcrumb from "@/components/atoms/Breadcrumb/BreadCrumbs";
+import MenuOutline from "@/assets/svgs/Menu.svg";
 import Button from "@/components/atoms/Button/Button";
-import TextInput from "@/components/atoms/Input/TextInput";
-import Header from "@/components/layouts/Header/Header";
-
-const breadcrumbs = [
-    {prefixIcon: <h1>Icon</h1>, label: "Home", url: "/home"},
-    {label: "About", url: "/about"},
-    {label: "Services", url: "/services"},
-    {label: "Projects", url: "/projects"},
-    {label: "Contact", url: "/contact"}
-];
 
 export default function Home() {
-    const handle = (e: any) => {
-        console.log(e);
-    };
     return (
         <>
-            <Header />
-            {/* <Button variant="file" onChange={handle} prefixIcon={<Bell />}>
-                Button title
-            </Button> */}
-            {/* <TextInput /> */}
-            <Breadcrumb breadcrumbs={breadcrumbs} />
+            <Button isIcon variant="primary-dark" color="white">
+                <MenuOutline />
+            </Button>
         </>
     );
 }
--- src/components/atoms/Button/Button.tsx
+++ src/components/atoms/Button/Button.tsx
@@ -18,12 +18,13 @@ interface PropsBtn {
         | "link"
         | "text";
     size: "large" | "medium" | "small";
-    color?: "blue" | "blue-dark";
+    color?: "white" | "blue" | "blue-dark";
     isDisabled: boolean;
     children?: ReactNode;
     prefixIcon?: ReactNode;
     url?: string;
     isError: boolean;
+    isIcon: boolean;
     className: string;
     onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
@@ -32,7 +33,9 @@ Button.defaultProps = {
     variant: "primary-light",
     size: "large",
     isDisabled: false,
-    isError: false
+    isError: false,
+    isIcon: false,
+    className: ""
 };
 
 export default function Button(
@@ -41,6 +44,7 @@ export default function Button(
         AnchorHTMLAttributes<HTMLAnchorElement>
 ) {
     const {
+        isIcon,
         variant,
         size,
         isDisabled,
@@ -65,9 +69,9 @@ export default function Button(
             case "dashed":
                 return "bg-white border border-stroke border-dashed hover:bg-highlight hover:border-primary-5-hover hover:text-primary-5-hover text-text";
             case "link":
-                return "bg-transparent text-primary hover:text-primary-5-hover";
+                return "bg-[transparent] text-primary hover:text-primary-5-hover";
             case "text":
-                return "bg-transparent text-text hover:bg-white";
+                return "bg-[transparent] text-text hover:bg-white";
             case "file":
             default:
                 return "";
@@ -86,7 +90,7 @@ export default function Button(
                     return "bg-white border border-dashed border-warning hover:border-warning-hover hover:text-warning-hover text-warning";
                 case "link":
                 case "text":
-                    return "bg-transparent text-warning hover:text-warning-hover";
+                    return "bg-[transparent] text-warning hover:text-warning-hover";
                 case "file":
                 default:
                     return "";
@@ -97,10 +101,12 @@ export default function Button(
     const getOutline = (color?: string) => {
         if (variant === "secondary" || variant === "dashed") {
             switch (color) {
+                case "white":
+                    return "bg-[transparent] border-white hover:border-white hover:bg-[transparent] hover:text-white text-white hover:opacity-60";
                 case "blue":
-                    return "border-secondary hover:border-secondary-hover hover:text-secondary-hover text-secondary";
+                    return "!border-secondary hover:!border-secondary-hover hover:!text-secondary-hover !text-secondary";
                 case "blue-dark":
-                    return "border-primary-5 hover:border-primary-5-hover hover:text-primary-5-hover text-primary-5";
+                    return "!border-primary-5 hover:!border-primary-5-hover hover:!text-primary-5-hover !text-primary-5";
                 default:
                     return "";
             }
@@ -111,11 +117,11 @@ export default function Button(
     const getSizeClass = (size: string) => {
         switch (size) {
             case "large":
-                return "min-h-[40px]";
+                return "min-w-[40px] min-h-[40px]";
             case "medium":
-                return "min-h-[32px]";
+                return "min-w-[32px] min-h-[32px]";
             case "small":
-                return "min-h-[22px]";
+                return "min-w-[22px] min-h-[22px]";
             default:
                 return "";
         }
@@ -131,7 +137,7 @@ export default function Button(
                 {...anchorProps}
                 href={url || ""}
                 className={`inline-flex justify-center items-center gap-[6px]
-                text-[14px] rounded-[3px] px-5 leading-[16.71px] font-medium transition ${className} ${getVariantClass(
+                text-[14px] rounded-[3px] px-5 leading-[16.71px] font-medium transition op ${className} ${getVariantClass(
                     variant
                 )} ${
                     isDisabled
@@ -139,18 +145,19 @@ export default function Button(
                         : ""
                 }  ${getErrorClass(variant)}`}
                 style={{fontFamily: "inherit"}}>
-                <span className="w-4 h-4">{prefixIcon}</span>
-                <span>{children}</span>
+                {prefixIcon && <span className="w-4 h-4">{prefixIcon}</span>}
+                <span className={`${isIcon ? "[&_svg]:w-6 [&_svg]:h-6" : ""}`}>
+                    {children}
+                </span>
             </Link>
         );
     }
-
     if (variant === "file") {
         return (
             <label
                 htmlFor="file-input"
                 className={`inline-flex justify-center items-center gap-[6px]
-              text-[14px] rounded-[3px] px-5 leading-[16.71px] font-medium transition cursor-pointer bg-white text-primary border border-stroke hover:bg-highlight hover:border-primary-5-hover ${className} ${getSizeClass(
+              text-[14px] rounded-[3px] px-5 leading-[16.71px] font-medium transition cursor-pointer bg-white text-primary border border-stroke hover:bg-highlight hover:border-primary-5-hover ${isIcon} ${className} ${getSizeClass(
                     size
                 )}`}>
                 <input type="file" id="file-input" hidden onChange={onChange} />
@@ -167,14 +174,16 @@ export default function Button(
             disabled={isDisabled}
             {...rest}
             className={`flex justify-center items-center gap-[6px]
-              text-[14px] rounded-[3px] px-5 leading-[16.71px] font-medium transition disabled:bg-disable disabled:text-input disabled:border disabled:border-stroke disabled:cursor-not-allowed ${className} ${getVariantClass(
+              text-[14px] rounded-[3px] px-5 leading-[16.71px] font-medium transition disabled:bg-disable disabled:text-input disabled:border disabled:border-stroke disabled:cursor-not-allowed ${
+                  isIcon ? "px-0" : ""
+              } ${className} ${getVariantClass(variant)} ${getErrorClass(
                 variant
-            )} ${getErrorClass(variant)} ${getSizeClass(size)} ${getOutline(
-                color
-            )}`}
+            )} ${getSizeClass(size)} ${getOutline(color)}`}
             style={{fontFamily: "inherit"}}>
-            <span className="w-4 h-4">{prefixIcon}</span>
-            <span>{children}</span>
+            {prefixIcon && <span className="w-4 h-4">{prefixIcon}</span>}
+            <span className={`${isIcon ? "[&_svg]:w-6 [&_svg]:h-6" : ""}`}>
+                {children}
+            </span>
         </button>
     );
 }
