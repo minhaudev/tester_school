670274caa89e82d2e90afcbeef411f6b1d5757f4
Duongbaxinh <duongbaxinh2021@gmail.com>
2024-07-15 16:47:43 +0700

    create navigation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,15 @@
 "use client";
+import Header from "@/components/layouts/Header/Header";
 
 import Navigation from "@/components/molecules/Navigation";
-<<<<<<< HEAD
 import TableFuture from "@/components/molecules/TableFuture";
-export default function Home() {
-    return (
-        <>
-            <Header />
-            <Navigation />
-            <TableFuture />
-=======
 import {NavigationType} from "@/interfaces";
 export default function Home() {
     return (
         <>
-            <Navigation type={NavigationType.MANAGE} />
->>>>>>> a25154b (create navigation)
+            <Header />
+            <Navigation type={NavigationType.CLIENT} />
+            <TableFuture />
         </>
     );
 }
