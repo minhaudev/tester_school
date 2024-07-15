d905f6627232cb98869cccfa13bd560a32e2b3ef
Duongbaxinh <duongbaxinh2021@gmail.com>
2024-07-15 16:57:06 +0700

    fix conflic

--- src/components/molecules/TableFuture/index.tsx
+++ src/components/molecules/TableFuture/index.tsx
@@ -1,19 +1,6 @@
 "use client";
-import ChartFill from "@/assets/svgs/Chart_fill.svg";
-import IconFilleDocAdd from "@/assets/svgs/File_dock_add_fill.svg";
-import FileDocFill from "@/assets/svgs/File_dock_fill.svg";
-import LineUp from "@/assets/svgs/Line_up.svg";
-import PaperFill from "@/assets/svgs/Paper_fill.svg";
-import UserCircle from "@/assets/svgs/User_cicrle.svg";
-import Database from "@/assets/svgs/Database_fill.svg";
-import TagPercent from "@/assets/svgs/Offer_Percent.svg";
-import FileCheck from "@/assets/svgs/File_Tick.svg";
-import Box3d from "@/assets/svgs/3d_box.svg";
-import UserAdd from "@/assets/svgs/User_add_alt_fill.svg";
-import GroupUser from "@/assets/svgs/manage_user-group.svg";
-import ProductUser from "@/assets/svgs/Product_User.svg";
 import IconListItem from "@/components/atoms/IconListItem";
-import tableFuture from "@/fake/tableFuture";
+import tableFuture from "@/faker/tableFuture";
 
 function TableFuture() {
     return (
