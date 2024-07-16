5cd6cfb264c1acdec787bb63626596d847a7a560
nguyenminhhau123 <hau95024@donga.edu.vn>
2024-07-16 08:25:45 +0700

    create interface input file

---
+++ src/interfaces/Input.ts
@@ -0,0 +1,26 @@
+import {ReactNode} from "react";
+
+export interface PropsInput {
+    variant?: "input" | "textarea";
+    size?: "large" | "medium" | "small";
+    label?: string;
+    value?: string | number;
+    type?: string;
+    error?: boolean;
+    helperText?: string;
+    suffixIcon?: string;
+    placeholder?: string;
+    name?: string;
+    handleOnChange?: (
+        e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+    ) => void;
+    isDisabled?: boolean;
+    className?: string;
+    prefix?: ReactNode;
+    suffix?: ReactNode;
+    require?: boolean;
+    showCalendar?: boolean;
+    setShowCalendar?: React.Dispatch<React.SetStateAction<boolean>>;
+    selectedDate?: Date | null;
+    handleDateChange?: (date: Date | null) => void;
+}
