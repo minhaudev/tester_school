08b41f1b5dfb89a47fc65b774be24050e24c02b0
nguyenminhhau123 <hau95024@donga.edu.vn>
2024-07-17 18:54:36 +0700

    fix: conflict

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,28 +2,12 @@
 import Header from "@/components/layouts/Header";
 
 import Navigation from "@/components/molecules/Navigation";
-import TableFuture from "@/components/molecules/TableFuture";
 import {NavigationType} from "@/interfaces";
-import {useState} from "react";
 export default function Home() {
-    // const [fileDetails, setFileDetails] = useState([]);
-
-    // const handleFileDetailsChange = (details: any) => {
-    //     setFileDetails(details);
-    // };
-
     return (
         <div className="flex w-full">
             <Navigation type={NavigationType.CLIENT} />
             <Header />
-            {/* <Button
-                size="semi"
-                variant="file"
-                color="white"
-                fileDetails={fileDetails}
-                setFileDetails={handleFileDetailsChange}>
-                Button
-            </Button> */}
             {/* <TableFuture /> */}
         </div>
     );
