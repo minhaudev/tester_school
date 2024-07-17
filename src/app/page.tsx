fdd4d7507fb038fc894e07a451ce0ea82531f96d
nguyenminhhau123 <hau95024@donga.edu.vn>
2024-07-17 08:53:26 +0700

    fix conflict button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import Button from "@/components/atoms/Button/Button";
+import Button from "@/components/atoms/Button";
 import Menu from "@/assets/svgs/Menu-1.svg";
 export default function Home() {
     return (
