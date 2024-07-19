c052c60490fa4d8e833ace0cf05ef8c07d9ffe15
Duongbaxinh <duongbaxinh2021@gmail.com>
2024-07-19 08:27:05 +0700

    create process flow

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,65 +1,12 @@
 "use client";
-import Header from "@/components/layouts/Header";
 
-import Navigation from "@/components/molecules/Navigation";
-import {NavigationType, ProcessFlowProps, stateProcess} from "@/interfaces";
-import LayoutContainer from "./LayoutContainer";
-import {useEffect, useState} from "react";
 import ProcessFlow from "@/components/molecules/ProcessFlow";
 import { processFlowData } from "@/faker";
+import LayoutContainer from "./LayoutContainer";
 export default function Home() {
-    type ActionType = "back" | "next";
-    const [statusProcess, setStateProcess] = useState(stateProcess.NONE);
-    const [flows, setFlow] = useState<ProcessFlowProps[]>(processFlowData);
-    const [currentProcessIndex, setCurrentProcessIndex] = useState(
-        processFlowData[0].id
-    );
-    const handleProcessFlow = ({
-        type,
-        index
-    }: {
-        type: ActionType;
-        index: number;
-    }) => {
-        if (type === "next" && currentProcessIndex < flows.length) {
-            let flowClone = [...flows];
-            const currentFlow = flowClone.findIndex((flow) => {
-                return flow.id === index;
-            });
-            if (currentFlow !== -1) {
-                flowClone.splice(currentFlow, 1, {
-                    ...flowClone[currentFlow],
-                    state: stateProcess.DONE
-                });
-                if (currentFlow + 1 < flowClone.length) {
-                    flowClone.splice(currentFlow + 1, 1, {
-                        ...flowClone[currentFlow + 1],
-                        state: stateProcess.ACTIVE
-                    });
-                    setCurrentProcessIndex(currentProcessIndex + 1);
-                }
-            }
-            setFlow([...flowClone]);
-        }
-        if(type === 'back' && currentProcessIndex > 1){
-            handleProcessActive(currentProcessIndex -1);
-        }
-    };
-    const handleProcessActive = (id:number)=>{
-        const cloneFlow = [...flows]
-       const updateFlow = cloneFlow.map((flow)=>{
-        if(flow.id === id){
-            flow.state = stateProcess.ACTIVE
-        }
-        if(flow.id > id){
-            flow.state = stateProcess.NONE
-        }
-        return flow
-       })
-       setFlow(()=>([...updateFlow]))
-       setCurrentProcessIndex(id)
+    const handleProcessActive = (id:number)=>{  
     }
     return <LayoutContainer>
-        <ProcessFlow onProcessActive={handleProcessActive}  flows={flows}/>
+        <ProcessFlow onProcessActive={handleProcessActive}  dataFlow={processFlowData}/>
     </LayoutContainer>;
 }
--- src/components/molecules/ProcessFlow/index.tsx
+++ src/components/molecules/ProcessFlow/index.tsx
@@ -1,12 +1,66 @@
+import Button from "@/components/atoms/Button";
 import ProcessFlowItem from "@/components/atoms/ProcessFlowItem";
-import {ProcessFlowProps} from "@/interfaces";
-function ProcessFlow({flows,onProcessActive}: {flows: ProcessFlowProps[],onProcessActive:(id:number)=>void}) {
-    
+import { processFlowData } from "@/faker";
+import {ProcessFlowProps, stateProcess} from "@/interfaces";
+import { useState } from "react";
+function ProcessFlow({dataFlow,onProcessActive}: {dataFlow: ProcessFlowProps[],onProcessActive:(id:number)=>void}) {
+    type ActionType = "back" | "next";
+    const [statusProcess, setStateProcess] = useState(stateProcess.NONE);
+    const [flows, setFlow] = useState<ProcessFlowProps[]>(processFlowData);
+    const [currentProcessIndex, setCurrentProcessIndex] = useState(
+        processFlowData[0].id
+    );
+    const handleProcessFlow = ({
+        type,
+        index
+    }: {
+        type: ActionType;
+        index: number;
+    }) => {
+        if (type === "next" && currentProcessIndex < flows.length) {
+            let flowClone = [...flows];
+            const currentFlow = flowClone.findIndex((flow) => {
+                return flow.id === index;
+            });
+            if (currentFlow !== -1) {
+                flowClone.splice(currentFlow, 1, {
+                    ...flowClone[currentFlow],
+                    state: stateProcess.DONE
+                });
+                if (currentFlow + 1 < flowClone.length) {
+                    flowClone.splice(currentFlow + 1, 1, {
+                        ...flowClone[currentFlow + 1],
+                        state: stateProcess.ACTIVE
+                    });
+                    setCurrentProcessIndex(currentProcessIndex + 1);
+                }
+            }
+            setFlow([...flowClone]);
+        }
+        if(type === 'back' && currentProcessIndex > 1){
+            handleProcessActive(currentProcessIndex -1);
+        }
+    };
+    const handleProcessActive = (id:number)=>{
+        const cloneFlow = [...flows]
+       const updateFlow = cloneFlow.map((flow)=>{
+        if(flow.id === id){
+            flow.state = stateProcess.ACTIVE
+        }
+        if(flow.id > id){
+            flow.state = stateProcess.NONE
+        }
+        return flow
+       })
+       setFlow(()=>([...updateFlow]))
+       setCurrentProcessIndex(id)
+    }
     return (
+        <>
         <div className="flex justify-start items-center gap-2">
             {flows && flows.map((flow) => (
                 <ProcessFlowItem
-                onChangeState={()=>onProcessActive(flow.id)}
+                onChangeState={()=>handleProcessActive(flow.id)}
                     key={flow.id}
                     id={flow.id}
                     icon={flow.icon}
@@ -16,6 +70,28 @@ function ProcessFlow({flows,onProcessActive}: {flows: ProcessFlowProps[],onProce
                 />
             ))}
         </div>
+        <div className="flex gap-4"> 
+        <button
+                onClick={() =>
+                    handleProcessFlow({
+                        type: "next",
+                        index: currentProcessIndex
+                    })
+                }>
+                next
+            </button>
+            <button
+                onClick={() =>
+                    handleProcessFlow({
+                        type: "back",
+                        index: currentProcessIndex
+                    })
+                }>
+               back
+            </button>
+        </div>
+        </>
+        
     );
 }
 
